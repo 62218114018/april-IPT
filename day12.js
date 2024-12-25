f37cafc07c1bee424989346c2416d665f96b82ec
62218114018 <navinmech2018@gmail.com>
2024-12-25 08:01:23 +0530

    First c

--- day12.js
+++ day12.js
@@ -4,10 +4,15 @@
 //     vehicle
 // }             
 
-let i = 1;
-do {
-  if (i % 2 !== 0) {
-    console.log(i); // Prints odd numbers from 1 to 10
-  }
-  i++;
-} while (i <= 10);
+// let i = 1;
+// do {
+//   if (i % 2 !== 0) {
+//     console.log(i); // Prints odd numbers from 1 to 10
+//   }
+//   i++;
+// } while (i <= 10);
+
+let a = 5
+let b = 3
+console.log(a+b);
+
